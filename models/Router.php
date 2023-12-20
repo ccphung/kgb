89ac6b188d0075a6c76e5248bbad785846e92450
Cédric Phung <cedric3011@outlook.fr>
2023-12-20 10:34:19 +0100

    modify router

--- models/Router.php
+++ models/Router.php
@@ -14,12 +14,9 @@ class Router {
 }
 public function getHandler(string $method, string $uri) { 
     foreach ($this->routes as $route) {
-        // Vérifiez si la méthode correspond
         if ($route['method'] === $method) {
-            // Utilisez une expression régulière pour comparer l'URI avec le chemin de la route
             $pattern = '~^' . $route['path'] . '$~';
             if (preg_match($pattern, $uri)) {
-                // Retournez le contrôleur et l'action si la route correspond
                 return [
                     'controller' => $route['controller'],
                     'action' => $route['action']
