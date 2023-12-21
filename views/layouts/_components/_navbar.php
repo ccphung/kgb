e3a9d8ea84ace227ae68c1a801983405fe0f5bfe
Cédric Phung <cedric3011@outlook.fr>
2023-12-21 14:41:44 +0100

    add navbar

--- views/layouts/_components/_navbar.php
+++ views/layouts/_components/_navbar.php
@@ -1,19 +1,26 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/home">KGB</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href='/home'>Accueil</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Agents</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/missions">Missions</a>
-      </li>
-    </ul>
+  <div class="container-fluid">
+      <a class="navbar-brand" href="/home">KGB</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link active" href='/home'>Accueil</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Agents</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="/missions">Missions</a>
+              </li>
+              <li class="nav-item login-nav">
+                  <a class="nav-link" href="/login">
+                      <i class="fa-solid fa-user"></i>
+                  </a>
+              </li>
+          </ul>
+      </div>
   </div>
 </nav>
--- views/layouts/default.php
+++ views/layouts/default.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap"    rel="stylesheet">
     <link rel="icon" type="image/png" href="../public/images/kgb.jpg" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="../public/css/styles.css">
     <title> <?= $title; ?> </title>
 </head>
@@ -16,6 +17,6 @@
     <?php require_once(BASE_URL.'/views/layouts/_components/_navbar.php');
     echo $content;
     ?>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
