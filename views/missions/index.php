58d89fe227e78eed6c3ca48856bd3779135e0f5a
Cédric Phung <cedric3011@outlook.fr>
2023-12-22 08:22:39 +0100

    add card style

--- views/missions/index.php
+++ views/missions/index.php
@@ -1,5 +1,16 @@
-<?php
-foreach ($missions as $mission): ?>
-<p><?=$mission['title']?></p>
-<a href="/missions/<?=$mission['id']?>">Accéder au détail de la mission</a>
-<?php endforeach; ?>
+<div class="container-fluid">
+    <div class="row">
+        <?php foreach ($missions as $mission): ?>
+            <div class="col m-3 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="../../public/images/top-secret.jpg" class="card-img-top" alt="top secret">
+                    <div class="card-body">
+                        <h5 class="card-title"><?=$mission['code_name']?></h5>
+                        <p class="card-text"><?=$mission['title']?></p>
+                        <a href="/missions/<?=$mission['id']?>" class="btn btn-secondary">Accéder au détail</a>
+                    </div>
+                    </div>
+                </div>
+        <?php endforeach; ?>        
+    </div>
+</div>
