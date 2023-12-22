cb7eb8874447aaa91ba59709c088cc014e949081
Cédric Phung <cedric3011@outlook.fr>
2023-12-22 21:14:09 +0100

    modify display

--- views/agents/index.php
+++ views/agents/index.php
@@ -1,4 +1,31 @@
-<?php 
-foreach ($agents as $agent): ?>
-    <?= $agent['first_name'] ?> <?= $agent['last_name'] ?> - <?= $agent['birth_date'] ?> - <?= $agent['name'] ?> - <?= $agent['agent_code'] ?>
-<?php endforeach; ?>
+<div class="container-fluid">
+    <div class="row">
+        <?php foreach ($agents as $agent): ?>
+            <div class="col m-3 d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="../../public/images/agent.jpg" class="card-img-top" alt="top secret">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $agent['first_name'] ?> <?= $agent['last_name'] ?></h5>
+                        <p class="card-text">
+                            <i class="fa-solid fa-cake-candles"></i>
+                            <?= $agent['birth_date'] ?>
+                        </p>
+                        <p class="card-text">
+                            <i class="fa-solid fa-globe"></i>
+                            <?= $agent['name'] ?>
+                        </p>
+                        <p class="card-text">
+                            <i class="fa-solid fa-barcode"></i>
+                            <?= $agent['agent_code'] ?>
+                        </p>
+                    </div>
+                </div>
+        </div>
+            <?php endforeach; ?>
+            </div>
+    </div>
+</div>
+
+
+
+
--- views/missions/detail.php
+++ views/missions/detail.php
@@ -20,6 +20,7 @@
         ?>"> 
         Statut : <?=$datas[0]['status']?>
     </h5>
+    
     <div class="d-flex justify-content-center">
         <div class="col-8 card m-2">
             <div class="card-body">
@@ -30,52 +31,7 @@
     </div>
 
     <div class="row d-flex justify-content-center">
-        <div class="col-8 col-md-5 card m-2">
-            <div class="card-body">
-                <h5 class="card-title">
-                    <i class="fa-solid fa-list"></i> 
-                    Détails
-                </h5>
-                <ul>
-                    <li>
-                        Type : <?= $datas[0]['mission_type']?>
-                    </li>
-                    <li>
-                        Localisation : <?= $datas[0]['mission_country']?>
-                    </li>
-                    <li>
-                        Spécialité requise : <?= $datas[0]['required_specialty']?>
-                    </li>
-                    <li class="card-text"> Dates : 
-                        <ul>
-                            <li class="card-text">
-                                Début : <?= $mission['start_date']?>
-                            </li>
-                            <?php if($datas[0]['status'] !== "En préparation" &&  $datas[0]['status'] !== "En cours") {
-                                echo "<li class='card-text'>Fin :" .  $mission['end_date'] ."</li>";
-                            }
-                            ?>
-
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-8 col-md-5 card m-2">
-            <div class="card-body">
-                <h5 class="card-title"><i class="fa-solid fa-house"></i></i> Planque</h5>
-                <ul>
-                    <li>Code : <?=$datas[0]['code'];?></li>
-                    <li>Adresse : <?=$datas[0]['address'];?></li>
-                    <li>Pays : <?=$datas[0]['stakeout_country'];?></li>
-                    <li>Type : <?=$datas[0]['type'];?></li>   
-                </ul>
-            </div>
-        </div>
-    </div>
-
-    <div class="row d-flex justify-content-center">
-        <div class="col-8 col-md-3 card m-2">
+        <div class="col-8 col-md-3 card m-2 bg-light">
             <div class="card-body">
                 <h5 class="card-title"><i class="fa-solid fa-user-tie"></i></i> Agents</h5>
                 <ul>
@@ -89,7 +45,7 @@
             </div>
         </div>
 
-        <div class="col-8 col-md-3  card m-2">
+        <div class="col-8 col-md-3 card m-2 bg-light">
             <div class="card-body">
                 <h5 class="card-title"><i class="fa-solid fa-bullseye"></i> Cibles</h5>
                 <ul>
@@ -102,7 +58,7 @@
                 </ul>
             </div>
         </div>
-        <div class="col-8 col-md-3  card m-2">
+        <div class="col-8 col-md-3 card m-2 bg-light">
             <div class="card-body">
                 <h5 class="card-title"><i class="fa-solid fa-address-book"></i> Contacts</h5>
                 <ul>
@@ -115,6 +71,51 @@
                 </ul>
             </div>
         </div>
+
+            <div class="row d-flex justify-content-center">
+                <div class="col-8 col-md-5 card m-2 bg-light">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <i class="fa-solid fa-list"></i> 
+                            Détails
+                        </h5>
+                        <ul>
+                            <li>
+                                Type : <?= $datas[0]['mission_type']?>
+                            </li>
+                            <li>
+                                Localisation : <?= $datas[0]['mission_country']?>
+                            </li>
+                            <li>
+                                Spécialité requise : <?= $datas[0]['required_specialty']?>
+                            </li>
+                            <li class="card-text"> Dates : 
+                                <ul>
+                                    <li class="card-text">
+                                        Début : <?= $mission['start_date']?>
+                                    </li>
+                                    <?php if($datas[0]['status'] !== "En préparation" &&  $datas[0]['status'] !== "En cours") {
+                                        echo "<li class='card-text'>Fin :" .  $mission['end_date'] ."</li>";
+                                    }
+                                    ?>
+
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-8 col-md-5 card m-2">
+                    <div class="card-body">
+                        <h5 class="card-title"><i class="fa-solid fa-house"></i></i> Planque</h5>
+                        <ul>
+                            <li>Code : <?=$datas[0]['code'];?></li>
+                            <li>Adresse : <?=$datas[0]['address'];?></li>
+                            <li>Pays : <?=$datas[0]['stakeout_country'];?></li>
+                            <li>Type : <?=$datas[0]['type'];?></li>   
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
