b4669512f0a3e21e038c4632bcc394753ab3dbfd
Cédric Phung <cedric3011@outlook.fr>
2023-12-29 17:34:04 +0100

    create new route

---
+++ views/agents/add.php
@@ -0,0 +1,9 @@
+<h1 class="text-center mt-5">Créer un agent</h1>
+
+<div class="container">
+    <div class="row d-flex justify-content-center">
+        <div class="col-6">
+            <?php echo $addAgentForm ?>
+        </div>
+    </div>
+</div>
