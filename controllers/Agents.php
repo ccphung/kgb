7261c47cf593a4e17b86612d9879f86687cd9eca
Cédric Phung <cedric3011@outlook.fr>
2023-12-29 18:21:57 +0100

    adjust indentation

--- controllers/Agents.php
+++ controllers/Agents.php
@@ -67,51 +67,51 @@ class Agents extends Controller {
             header("Location: /agents");
             exit();
             }
-        
-    }}
-            public function createForm() {
-                $countries = $this->Agent->getCountries();
-                $specialties = $this->Agent->getSpecialties();
-                $title = "Ajout agent";
+        }
+    }   
+    public function createForm() {
+        $countries = $this->Agent->getCountries();
+        $specialties = $this->Agent->getSpecialties();
+        $title = "Ajout agent";
 
-                //Check if user is connected
-                if(isset($_SESSION['user']) && !empty($_SESSION['user']['id'])){
+        //Check if user is connected
+        if(isset($_SESSION['user']) && !empty($_SESSION['user']['id'])){
 
-                $form = new Form;
+            $form = new Form;
 
-                $form->debutForm('POST', '/agents/post')
-                    ->addLabelFor('firstName', 'Prénom :')
-                    ->addInput('text', 'firstName', ['class' => 'form-control required'])
+            $form->debutForm('POST', '/agents/post')
+                ->addLabelFor('firstName', 'Prénom :')
+                ->addInput('text', 'firstName', ['class' => 'form-control required'])
 
-                    ->addLabelFor('lastName', 'Nom :')
-                    ->addInput('text', 'lastName', ['class' => 'form-control'])
+                ->addLabelFor('lastName', 'Nom :')
+                ->addInput('text', 'lastName', ['class' => 'form-control'])
 
-                    ->addLabelFor('birthDate', 'Date de naissance :')
-                    ->addInput('date', 'birthDate', ['class' => 'form-control'])
+                ->addLabelFor('birthDate', 'Date de naissance :')
+                ->addInput('date', 'birthDate', ['class' => 'form-control'])
 
-                    ->addLabelFor('country', 'Nationalité :')
-                    ->addSelect('country', array_column($countries, 'name', 'id'), ['id' => 'country', 'class' => 'form-control'])
+                ->addLabelFor('country', 'Nationalité :')
+                ->addSelect('country', array_column($countries, 'name', 'id'), ['id' => 'country', 'class' => 'form-control'])
 
-                    ->addLabelFor('agentCode', 'Code agent :')
-                    ->addInput('number', 'agentCode', ['class' => 'form-control'])
+                ->addLabelFor('agentCode', 'Code agent :')
+                ->addInput('number', 'agentCode', ['class' => 'form-control'])
 
-                    ->addLabelFor('specialty', 'Spécialité :');
-                    foreach ($specialties as $key => $specialty) {
-                        $specialtyName = $specialty['name'];
-                    
-                        $form->addInput('checkbox', 'specialties[]', ['class' => 'form-check-input mt-3 m-2'], $key)
-                            ->addLabelFor($key, $specialtyName, ['class' => 'form-check-label mt-3 required']);
-                    }
+                ->addLabelFor('specialty', 'Spécialité :');
+                foreach ($specialties as $key => $specialty) {
+                    $specialtyName = $specialty['name'];
+                
+                    $form->addInput('checkbox', 'specialties[]', ['class' => 'form-check-input mt-3 m-2'], $key)
+                        ->addLabelFor($key, $specialtyName, ['class' => 'form-check-label mt-3 required']);
+                }
 
-                    $form->addButton('Créer', ['class' => 'btn btn-primary mt-2 col-12'])
-                    ->endForm();
+                $form->addButton('Créer', ['class' => 'btn btn-primary mt-2 col-12'])
+                ->endForm();
 
-                $this->render('add', ['countries' => $countries, 'title' => $title, 'addAgentForm' => $form->create()], );
+            $this->render('add', ['countries' => $countries, 'title' => $title, 'addAgentForm' => $form->create()], );
 
-           } else {
+        } else {
             $_SESSION['error_message'] = "Vous devez être connecté(e) pour accéder à cette page";
             header('Location: /login');
             exit();
-           }
         }
-    }    
+    }
+}    
