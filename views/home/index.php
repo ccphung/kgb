3421e9e686d5e26007f0335ee224b03e304ef450
Cédric Phung <cedric3011@outlook.fr>
2024-01-04 11:43:55 +0100

    modify display

--- views/home/index.php
+++ views/home/index.php
@@ -8,66 +8,41 @@
 
 <div class="container-fluid">
     <div class="row d-flex justify-content-center">
-        <div class="col-lg-3 col-md-6 m-2">
+        <div class="col-5 m-2">
             <div class="card bg-dark text-white" style="width: 18rem;">
                 <img class="card-img-top" src="../../public/images/agent.jpg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Agents</h5>
-                    <ul>
-                        <li class="card-text">Nom</li>
-                        <li class="card-text">Spécialités</li>
-                        <li class="card-text">Date de naissance</li>
-                        <li class="card-text">Pays</li>
-                    </ul>
-
                     <a href="/agents" class="btn btn-primary">Liste des agents</a>
                 </div>
             </div>
         </div>
-        <div class="col-lg-3 col-md-6 m-2">
+        <div class="col-5 m-2">
             <div class="card bg-dark text-white" style="width: 18rem;">
                 <img class="card-img-top" src="../../public/images/target.png" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Cibles</h5>
-                    <ul>    
-                        <li class="card-text">Nom</li>
-                        <li class="card-text">Date de naissance</li>
-                        <li class="card-text">Pays</li>
-                    </ul>
                     <a href="/targets" class="btn btn-primary">Liste des cibles</a>
                 </div>
             </div>
         </div>
-        <div class="col-lg-3 col-md-6 m-2">
+        <div class="col-5 m-2">
             <div class="card bg-dark text-white" style="width: 18rem;">
                 <img class="card-img-top" src="../../public/images/contact.jpg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Contacts</h5>
-                    <ul>    
-                        <li class="card-text">Nom</li>
-                        <li class="card-text">Date de naissance</li>
-                        <li class="card-text">Pays</li>
-                    </ul>
                     <a href="/contacts" class="btn btn-primary">Liste des contacts</a>
                 </div>
             </div>
         </div>
-
-        <div class="col-lg-3 col-md-6 m-2">
+        <div class="col-5 m-2">
             <div class="card bg-dark text-white" style="width: 18rem;">
                 <img class="card-img-top" src="../../public/images/mission.jpg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Mssions</h5>
-                    <ul>    
-                        <li class="card-text">Détails</li>
-                        <li class="card-text">Status</li>
-                        <li class="card-text">Planque</li>
-                        <li class="card-text">Agents/Cibles/Contacts</li>
-                    </ul>
                     <a href="/missions" class="btn btn-primary">Liste des missions</a>
                 </div>
             </div>
-        </div>
         </div>
     </div>
 </div>
