234f15159226950665aa42e7b332e9945877e0a7
Cédric Phung <cedric3011@outlook.fr>
2024-01-07 21:28:40 +0100

    modify display

--- views/admins/index.php
+++ views/admins/index.php
@@ -1,8 +1,16 @@
-<h1>Espace d'administration</h1>
+<h1 class="text-center mt-2">Espace d'administration</h1>
 
-<ul>
-    <li><a href="/admin/missions">Gérer les missions</a></li>
-    <li><a href="/admin/agents">Gérer les agents</a></li>
-    <li><a href="/admin/contacts">Gérer les contacts</a></li>
-    <li><a href="/admin/cibles">Gérer les cibles</a></li>
+<ul class="d-flex justify-content-center mt-5">
+    <li class="list-unstyled">
+        <a class="btn btn-primary m-2" href="/admin/missions">Gérer les missions</a>
+    </li>
+    <li class="list-unstyled">
+        <a class="btn btn-primary m-2" href="/admin/agents">Gérer les agents</a>
+    </li>
+    <li class="list-unstyled">
+        <a class="btn btn-primary m-2" href="/admin/contacts">Gérer les contacts</a>
+    </li>
+    <li class="list-unstyled">
+        <a class="btn btn-primary m-2" href="/admin/cibles">Gérer les cibles</a>
+    </li>
 </ul>
